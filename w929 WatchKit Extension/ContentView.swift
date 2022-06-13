8fc4edc9f6dcf01c3159aa1b958970294e9a1630
Yotam0Lichter <76899692+Yotam0Lichter@users.noreply.github.com>
2022-06-13 19:50:31 +0300

    Initial Commit

---
+++ w929 WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  w929 WatchKit Extension
+//
+//  Created by Yotam Lichter on 13/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ w929 WatchKit Extension/w929App.swift
@@ -0,0 +1,19 @@
+//
+//  w929App.swift
+//  w929 WatchKit Extension
+//
+//  Created by Yotam Lichter on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct w929App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
