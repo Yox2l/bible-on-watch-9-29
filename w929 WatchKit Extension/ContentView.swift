4bfa009f8930c91d87452743cab64ac15aa5fe12
Yotam Lichter <yotmail@gmail.com>
2022-06-30 05:41:29 +0000

    Adding bottun colors

--- w929 WatchKit Extension/ContentView.swift
+++ w929 WatchKit Extension/ContentView.swift
@@ -13,16 +13,19 @@ var DAILY = "הפרק היומי"
 struct Chapter: Decodable, Identifiable {
     let id: String
     let index: Int
+    let color: String
 }
 
 struct Book: Decodable, Identifiable {
     let id: String
     let chapters: [Chapter]
+    let color: String
 }
 
 struct BookGroup: Decodable, Identifiable {
     let id: String
     let books: [Book]
+    let color: String
 }
 
 struct CahpterGuide: Decodable {
@@ -54,6 +57,11 @@ func getColor(r: Double, g: Double, b: Double) -> Color {
     return Color(red: r / 255, green: g / 255, blue: b / 255)
 }
 
+func strToColor(clrStr: String) -> Color {
+    let sp = clrStr.split(separator: ",")
+    return getColor(r: Double(sp[0])!, g: Double(sp[1])!, b: Double(sp[2])!)
+}
+
 var bookGroups:[BookGroup] =  load("app_data/map.json")
 var chapterGuide:[CahpterGuide] =  load("app_data/capter_guide.json")
 
@@ -72,31 +80,36 @@ struct ContentView: View {
                 ScrollViewReader { value in
                     if curBookGroup != "" {
                         HStack {
+                            if prevTitle != "" {
+                                Button {
+                                    if prevTitle == curBookGroup {
+                                        curBook = ""
+                                        prevTitle = ""
+                                    } else {
+                                        loadChapter(newPos: pos - 1)
+                                    }
+                                } label: {
+                                    Label(prevTitle, systemImage: "arrow.backward").frame(minWidth: 85)
+                                }.background(getColor(r: 255, g: 215, b:97)).cornerRadius(8)
+                            }
                             Button {
                                 curBookGroup = ""
                                 curBook = ""
                                 viewText = ""
                                 title = HOME
+                                prevTitle = ""
                             } label: {
                                 Label(HOME, systemImage: "menucard")
-                            }.background(getColor(r: 255, g: 66, b:161)).cornerRadius(8)
-                            if viewText != "" && prevTitle != "" {
-                                Button {
-                                    loadChapter(newPos: pos - 1)
-                                    value.scrollTo(0)
-                                } label: {
-                                    Label(prevTitle, systemImage: "arrow.forward")
-                                }.background(getColor(r: 254, g: 174, b:0)).cornerRadius(8)
-                            }
+                            }.background(getColor(r: 215, g: 97, b:255)).cornerRadius(8)
                         }.id(0)
                     } else {
                         Button {
                             curBookGroup = "Daily"
                             loadDailyChapter()
                         } label: {
-                            Label(DAILY, systemImage: "pencil")
+                            Label(DAILY, systemImage: "clock")
                         }
-                        .background(getColor(r: 0, g: 161, b:255)).cornerRadius(8)
+                        .background(getColor(r: 97, g: 255, b:255)).cornerRadius(8)
                     }
                     Spacer(minLength: 20)
                     if viewText == "" {
@@ -105,7 +118,7 @@ struct ContentView: View {
                                 Button(bookGroup.id) {
                                     curBookGroup = bookGroup.id
                                     title = bookGroup.id
-                                }
+                                }.background(strToColor(clrStr: bookGroup.color)).cornerRadius(8)
                             }
                             if curBookGroup == bookGroup.id {
                                 ForEach(bookGroup.books) { book in
@@ -113,15 +126,16 @@ struct ContentView: View {
                                         Button(book.id) {
                                             curBook = book.id
                                             title = book.id
+                                            prevTitle = curBookGroup
                                             value.scrollTo(0)
-                                        }
+                                        }.background(strToColor(clrStr: book.color)).cornerRadius(8)
                                     }
                                     if curBook == book.id {
                                         ForEach(book.chapters) { chapter in
                                             Button(chapter.id) {
                                                 loadChapter(newPos: chapter.index)
                                                 value.scrollTo(0)
-                                            }
+                                            }.background(strToColor(clrStr: chapter.color)).cornerRadius(8)
                                         }
                                     }
 
@@ -129,14 +143,14 @@ struct ContentView: View {
                             }
                         }
                     } else {
-                        Text(viewText).multilineTextAlignment(.trailing).lineLimit(nil)
+                        Text(viewText).lineLimit(nil)
                         if nextTitle != "" {
                             Button {
                                 loadChapter(newPos: pos + 1)
                                 value.scrollTo(0)
                             } label: {
-                                Label(nextTitle, systemImage: "arrow.backward")
-                            }.background(getColor(r: 254, g: 174, b:0)).cornerRadius(8)
+                                Label(nextTitle, systemImage: "arrow.forward")
+                            }.background(getColor(r: 255, g: 215, b:97)).cornerRadius(8)
                         }
                     }
                 }
